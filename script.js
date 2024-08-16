5399d1a6382f736a882665f96bb09a2951c732d9
Nthabiseng Mashalane <nthabimashalane0@gmail.com>
2024-08-16 14:52:00 +0200

    Changes

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+    let result = '';
+    let validateButton = document.getElementById('circle-Btn');
+
 
     // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
 
-            document.getElementById('result').innerText = result;
-}
+        const adopt = /^pet_[a-zA-Z0-9]+$/;
+      
+    // Check if the input matches the adopt
+
+    if (adopt.test(input)) {
+        result = "Valid Syntax.";
+        validateButton.style.backgroundColor ='green';
+    } else {
+        result = "Invalid Syntax.";
+        validateButton.style.backgroundColor ='red';
+    }
+        document.getElementById('result').innerText = result; 
+    }
+
+    
+    
+
+
+    
 
 
